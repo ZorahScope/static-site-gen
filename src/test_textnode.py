70dbbbb0dc3517555d9203542a34094e7b9d5e80
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-05-27 14:31:49 -0500

    Complete lesson on unit tests

---
+++ src/test_textnode.py
@@ -0,0 +1,43 @@
+import unittest
+
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_link,
+    text_type_image,
+)
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+
+    def test_eq_false(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_bold)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node2", text_type_text)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.example.com")
+        node2 = TextNode("This is a text node", text_type_text, "https://www.example.com")
+        self.assertEqual(node, node2)
+        
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()

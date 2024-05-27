6aee0638d047d33e04a1b3a13dfaeed5d292f8d1
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-05-27 14:36:04 -0500

    Fix: ran code formatter

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from textnode import TextNode
 
+
 def main():
     test_obj = TextNode("Hello World", "bold", "https://www.example.com")
     print(test_obj)
--- src/textnode.py
+++ src/textnode.py
@@ -5,6 +5,7 @@ text_type_code = "code"
 text_type_link = "link"
 text_type_image = "image"
 
+
 class TextNode:
     def __init__(self, text, text_type, url=None):
         self.text = text
@@ -13,11 +14,10 @@ class TextNode:
 
     def __eq__(self, other):
         return (
-            self.text == other.text and
-            self.text_type == other.text_type and
-            self.url == other.url
+                self.text == other.text and
+                self.text_type == other.text_type and
+                self.url == other.url
         )
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
-

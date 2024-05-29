701ab6dc539933432483e5a78de96178660f55f8
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-05-28 19:19:05 -0500

    Override LeafNode class method

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -40,3 +40,6 @@ class LeafNode(HTMLNode):
             return self.value
 
         return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+
+    def __repr__(self):
+        return f'LeafNode({self.tag}, {self.value}, {self.props})'

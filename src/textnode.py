aab1e795aef3d1f8ae599926d1e4090320c3c040
Zorah <65967638+ZorahScope@users.noreply.github.com>
2024-05-31 12:34:22 -0500

    Add converter text_note_to_html_node function
    Also implemented a enum to handle the accepted types of TextNodes

--- src/textnode.py
+++ src/textnode.py
@@ -1,9 +1,5 @@
-text_type_text = "text"
-text_type_bold = "bold"
-text_type_italic = "italic"
-text_type_code = "code"
-text_type_link = "link"
-text_type_image = "image"
+from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextNode:
@@ -14,6 +10,7 @@ class TextNode:
 
     def __eq__(self, other):
         return (
+                isinstance(other, TextNode) and
                 self.text == other.text and
                 self.text_type == other.text_type and
                 self.url == other.url
@@ -21,3 +18,39 @@ class TextNode:
 
     def __repr__(self):
         return f'TextNode({self.text}, {self.text_type}, {self.url})'
+
+
+class TextType(Enum):
+    TEXT = 1
+    BOLD = 2
+    ITALIC = 3
+    CODE = 4
+    LINK = 5
+    IMAGE = 6
+
+    def __eq__(self, other):
+        if isinstance(other, TextType):
+            return self.value == other.value
+        if isinstance(other, int):
+            return self.value == other
+        if isinstance(other, str):
+            return self.name.lower() == other.lower()
+        return super().__eq__(other)
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(text_node.text)
+        case TextType.BOLD:
+            return LeafNode(text_node.text, "b")
+        case TextType.ITALIC:
+            return LeafNode(text_node.text, "i")
+        case TextType.CODE:
+            return LeafNode(text_node.text, "code")
+        case TextType.LINK:
+            return LeafNode(text_node.text, "a", {'href': text_node.url})
+        case TextType.IMAGE:
+            return LeafNode('', "img", {'src': text_node.url, 'alt': text_node.text})
+        case _:
+            raise TypeError(f'Invalid text type')
